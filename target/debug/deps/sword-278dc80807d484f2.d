6be910c90f4d9d2e2904f2e8741934c5ae0fc9d5
kjuulh <contact@kjuulh.io>
2023-02-25 11:25:48 +0100

    remove target

--- target/debug/deps/sword-278dc80807d484f2.d
+++
@@ -1,5 +0,0 @@
-/Users/kah/git/github.com/kjuulh/sword/target/debug/deps/sword-278dc80807d484f2.rmeta: crates/sword/src/main.rs
-
-/Users/kah/git/github.com/kjuulh/sword/target/debug/deps/sword-278dc80807d484f2.d: crates/sword/src/main.rs
-
-crates/sword/src/main.rs:
--- target/debug/sword.d
+++
@@ -1,1 +0,0 @@
-/Users/kah/git/github.com/kjuulh/sword/target/debug/sword: /Users/kah/git/github.com/kjuulh/sword/crates/sword/src/main.rs
